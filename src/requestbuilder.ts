e360c669d33fc3ed2769474da6946546a94612c4
Levin Rüßmann <Levin13c@gmail.com>
2026-04-05 16:32:33 +0200

    request builder fertig

--- src/requestbuilder.ts
+++ src/requestbuilder.ts
@@ -4,10 +4,12 @@ import { API_CONFIG } from "./config.js";
 const base_url = API_CONFIG.baseUrl + "/" + API_CONFIG.userPrefix + "/";
 
 export const RestEndpoints = {
-  items: base_url + "items",
   collections: base_url + "collections",
+  items: base_url + "items",
   tags: base_url + "tags",
 };
+
+// Collections Endpoints
 
 // Alle Collections abrufen
 export async function getCollections() {
@@ -39,6 +41,77 @@ export async function getCollectionByKey(key: string) {
 // Untercollections einer Collection mit dem Key abrufen
 export async function getSubcollectinsByKey(key: string) {
   const url = new URL(RestEndpoints.collections) + "/" + key + "/collections";
+  const response = await fetch(url, {
+    method: "GET",
+  });
+  return response.json();
+}
+
+// Items einer Collection mit dem Key abrufen
+export async function getItemsByCollectionKey(key: string) {
+  const url = new URL(RestEndpoints.collections) + "/" + key + "/items";
+  const response = await fetch(url, {
+    method: "GET",
+  });
+  return response.json();
+}
+
+// Top Items einer Collection mit dem Key abrufen
+export async function getTopItemsByCollectionKey(key: string) {
+  const url = new URL(RestEndpoints.collections) + "/" + key + "/items/top";
+  const response = await fetch(url, {
+    method: "GET",
+  });
+  return response.json();
+}
+
+// Item Endpoints
+export async function getItems() {
+  const url = new URL(RestEndpoints.items);
+  const response = await fetch(url, {
+    method: "GET",
+  });
+  return response.json();
+}
+
+export async function getItemsTags() {
+  const url = new URL(RestEndpoints.items) + "/tags";
+  const response = await fetch(url, {
+    method: "GET",
+  });
+  return response.json();
+}
+
+export async function getItemByKey(key: string) {
+  const url = new URL(RestEndpoints.items) + "/" + key;
+  const response = await fetch(url, {
+    method: "GET",
+  });
+  return response.json();
+}
+
+export async function getItemChildrenByKey(key: string) {
+  const url = new URL(RestEndpoints.items) + "/" + key + "/children";
+  const response = await fetch(url, {
+    method: "GET",
+  });
+  return response.json();
+}
+
+// tag of Item mit Key abrufen
+export async function getTagsByItemKey(key: string) {
+  const url = new URL(RestEndpoints.items) + "/" + key + "/tags";
+  const response = await fetch(url, {
+    method: "GET",
+  });
+  return response.json();
+}
+
+// Tags Endpoints
+
+// Alle Tags abrufen
+export async function getTags() {
+  const url = new URL(RestEndpoints.tags);
   const response = await fetch(url, {
     method: "GET",
   });
